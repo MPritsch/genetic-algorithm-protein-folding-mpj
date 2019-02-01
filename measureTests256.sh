a6f0c2cd0f02e490a51cf0da57673991e9c924de
Marcus Pritsch <marcus.pritsch@stud.h-da.de>
2019-02-01 19:14:18 +0100

    Fix script

--- measureTests256.sh
+++ measureTests256.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 ga_alg() {
-    N=$1
-    PROCESSES=$2
-    GENERATIONS=$3
+    PROCESSES=$1
+    GENERATIONS=$2
 
     echo -e "\n\n\n----------------------- NEXT ------------------------\n\n\n"
-    echo -e "Using $N nodes with $PROCESSES processes calculating $GENERATIONS generations per process\n"
+    echo -e "Using any amount of nodes with $PROCESSES processes calculating $GENERATIONS generations per process\n"
     echo -e "\nRun 1\n\n"
-    salloc -N ${N} -n ${PROCESSES} mpjrun.sh -np ${PROCESSES} -dev niodev \
+    salloc -n ${PROCESSES} mpjrun.sh -np ${PROCESSES} -dev niodev \
         -jar ~/java-mpj/genetic-algorithm-protein-folding-mpj-1.0-SNAPSHOT-jar-with-dependencies.jar --generation ${GENERATIONS}
     echo -e "\nRun 2\n\n"
-    salloc -N ${N} -n ${PROCESSES} mpjrun.sh -np ${PROCESSES} -dev niodev \
+    salloc -n ${PROCESSES} mpjrun.sh -np ${PROCESSES} -dev niodev \
         -jar ~/java-mpj/genetic-algorithm-protein-folding-mpj-1.0-SNAPSHOT-jar-with-dependencies.jar --generation ${GENERATIONS}
     echo -e "\nRun 3\n\n"
-    salloc -N ${N} -n ${PROCESSES} mpjrun.sh -np ${PROCESSES} -dev niodev \
+    salloc -n ${PROCESSES} mpjrun.sh -np ${PROCESSES} -dev niodev \
         -jar ~/java-mpj/genetic-algorithm-protein-folding-mpj-1.0-SNAPSHOT-jar-with-dependencies.jar --generation ${GENERATIONS}
 }
 
@@ -22,22 +21,16 @@ runtests() {
     echo -e "\n\n\n##################### NEW TEST SUITE ##########################\n\n\n"
 
     CORE_COUNT_ARRAY=($@)
-    N=$2
 
-    echo -e "Using $CORE_COUNT_ARRAY cores on $N hosts"
+    echo -e "Using $CORE_COUNT_ARRAY cores"
 
-    for i in "${CORE_COUNT_ARRAY[@]}"
-    do
-        ga_alg $N $i 10
-        ga_alg $N $i 20
-        ga_alg $N $i 40
-        ga_alg $N $i 80
-        ga_alg $N $i 160
-    done
+    ga_alg 256 10
+    ga_alg 256 20
+    ga_alg 256 40
+    ga_alg 256 80
+    ga_alg 256 160
 }
 
 echo -e "time in ms;mpj process amount;total generation count;total calculated individuals;time limit;generation limit;given commands;\n" >> results.csv
 
-UNTIL_128=( 1 2 4 8 16 28 32 56 64 128 )
-
-runtests ${UNTIL_128[@]} 4
+runtests
